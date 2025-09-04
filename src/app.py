dd14ad5575812df2ca88488cd673eb08d0975fa7
estiver <estiverherrerahipolito@gmail.com>
2025-09-03 22:00:07 -0300

    git acctions

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def details():
     return jsonify({
         'time': datetime.now(),
-        'hostname': socket.gethostname()
+        'hostname': socket.gethostname(),
+        'message': 'You are doing great, human!'
     })
 
 @app.route('/api/v1/healthz')
